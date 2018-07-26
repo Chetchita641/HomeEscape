dcc678cdfacf4c272d1f4e69ea0ba677488d87b8
Chris Macholtz <cmacholtz@jogusa-chris.joganic.com>
2018-07-26 10:09:11 -0700

    first commit

---
+++ Source/HomeEscape/HomeEscapeGameModeBase.h
@@ -0,0 +1,20 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "HomeEscapeGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class HOMEESCAPE_API AHomeEscapeGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+	
+	
+	
+};
